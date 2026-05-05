3fcc1daaacb05dd77f99864581a2b9c1990a6b89
rochpams <name>
2026-05-05 20:17:47 +0800

    okayy

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,8 +11,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->trustProxies(at: '*');
-
         $middleware->alias([
             'admin' => \App\Http\Middleware\CheckAdmin::class,
             'hr' => \App\Http\Middleware\CheckHR::class,
--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\URL; 
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if (app()->environment('production')) {
-            URL::forceScheme('https'); 
+            URL::forceScheme('https');
         }
     }
 }
